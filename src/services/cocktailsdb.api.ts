8761dfd53cf1f6b806c49803a780da1467fd3670
snstrauss <zahavi.guy.85@gmail.com>
2025-03-18 17:59:53 +0200

    added simple service to fetch all cocktails by initial char

---
+++ src/services/cocktailsdb.api.ts
@@ -0,0 +1,18 @@
+import { RawCocktailsResponse } from "../types/cocktails";
+
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1";
+
+function getJson(url: string) {
+  return fetch(url).then((res) => res.json());
+}
+
+const allLetters = 'abcdefghijklmnopqrstuvwxyz';
+const allDigits = '0123456789';
+
+export function fetchAllCocktails(): Promise<RawCocktailsResponse> {
+  return Promise.all(
+    [...allLetters, ...allDigits].map((letter) => {
+      return getJson(`${BASE_URL}/search.php?f=${letter}`);
+    })
+  );
+}
